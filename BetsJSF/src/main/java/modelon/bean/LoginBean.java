57ac51b2a3452ee3e07131394afc6878454aa4da
YuboChen20 <100442328+YuboChen20@users.noreply.github.com>
2022-11-27 13:34:01 +0100

    fff

---
+++ BetsJSF/src/main/java/modelon/bean/LoginBean.java
@@ -0,0 +1,30 @@
+package modelon.bean;
+
+public class LoginBean {
+	private String nombre;
+	private String password;
+
+	public String getNombre() {
+	 return nombre;
+	 }
+	public void setNombre(String nombre) {
+	 this.nombre = nombre;
+	 }
+	public String getPassword() {
+	 return password;
+	 }
+	public void setPassword(String password) {
+	 this.password = password;
+	 }
+
+	public String comprobar() {
+	 if(nombre.equals("Admin")){
+	 return "okAdmin";
+	 }
+	 else {
+	 return "okUser";
+	 }
+	 } 
+
+
+}
