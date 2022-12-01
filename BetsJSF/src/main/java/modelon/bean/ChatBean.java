79b18a842c487516462437a54ee548c837a0c30c
EHU <a@a.com>
2022-12-01 18:16:41 +0100

    Version Chat Beta

--- BetsJSF/src/main/java/modelon/bean/ChatBean.java
+++ BetsJSF/src/main/java/modelon/bean/ChatBean.java
@@ -10,6 +10,7 @@ import modelon.dominio.Usuario;
 public class ChatBean {
 	private String text;
 	private ArrayList<Comentario> chat = new ArrayList<Comentario>();
+	private String ult;
 	
 	public ChatBean() {}
 
@@ -21,13 +22,48 @@ public class ChatBean {
 		this.text = text;
 	}
 	
+	
+	public ArrayList<Comentario> getChat() {
+		return chat;
+	}
+
+	public void setChat(ArrayList<Comentario> chat) {
+		this.chat = chat;
+	}
+	
+	
+
+	public String getUlt() {
+		return ult;
+	}
+
+	public void setUlt(String ult) {
+		this.ult = ult;
+	}
+
 	public void sendComment() {
 		Usuario user = new Usuario("ChicoGuapo", "123", "123456789112", "pepe@pepa.pig", false);
 		Event ev1=new Event(1, "Atlético-Athletic", UtilDate.newDate(2001,12,17));
 		int num = chat.size();
 		Comentario com = new Comentario(num, text,ev1,user,"Mañana");
+		this.setText("");
 		System.out.println(com);
 		chat.add(com);
+		if(ult==null) {
+			ult=com.toString();
+		}else {
+			ult=ult+"\n"+com.toString();
+		}
+		
+	}
+	
+	public void viewScreen() {
+		if(!chat.isEmpty()) {
+			for(Comentario com:chat) {
+				System.out.println(com);
+				System.out.println("Pasa");
+			}
+		}
 	}
 	
 }
